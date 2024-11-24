cc01c48a080c1a70074483bdddc3042c24525a69
Kirill <kirillkorolev2002@mail.ru>
2024-11-24 23:03:25 +0300

    Initial Commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "KotlinTelegramBot"
-
+include("src:main:KotlinTelegramBot")
+findProject(":src:main:KotlinTelegramBot")?.name = "KotlinTelegramBot"

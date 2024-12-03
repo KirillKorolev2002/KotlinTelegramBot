52732c2bbef7c244b676b4c8ecd604ca3e7d77ea
Kirill <kirillkorolev2002@mail.ru>
2024-12-03 23:24:03 +0300

    KTB-04-01-Parcing_1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun main() {
         val parts = it.split("|")
         val original = parts[0]
         val translation = parts[1]
-        val correctAnswersCount = parts.toIntOrNull() ?: 0
+        val correctAnswersCount = parts.getOrNull(2)?.toIntOrNull() ?: 0
         dictionary.add(Word(original, translation, correctAnswersCount))
     }
     dictionary.forEach { println("${it.original} - ${it.translation} - ${it.correctAnswersCount}") }

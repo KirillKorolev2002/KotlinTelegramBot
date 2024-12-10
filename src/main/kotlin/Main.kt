8c0faf4bd875e712da4986a81b13ecbcdf3cb426
Kirill <kirillkorolev2002@mail.ru>
2024-12-10 17:32:59 +0300

    KTB-05-01-1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val dictionary = loadDictionary() //Загрузка словаря
+
+    while (true) {
+        println("Меню:")
+        println("1 – Учить слова")
+        println("2 – Статистика")
+        println("0 – Выход")
+
+        val input = readln()
+
+        when (input) {
+            "1" -> println("Выбран пункт 'Учить слова'")
+            "2" -> println("Выбран пункт 'Статистика'")
+            "0" -> break
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
+}

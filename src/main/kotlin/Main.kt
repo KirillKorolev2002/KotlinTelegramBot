5fd40ffc70661219138ba86a248ef9e5c1d516ac
Kirill <kirillkorolev2002@mail.ru>
2024-12-15 20:36:50 +0300

    KTB-07-01

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,25 @@
 import java.io.File
 
 fun main() {
+    val dictionary = loadDictionary()
+    while (true) {
+        println("Меню:")
+        println("1 – Учить слова")
+        println("2 – Статистика")
+        println("0 – Выход")
+
+        val input = readln()
+
+        when (input) {
+            "1" -> println("Выбран пункт 'Учить слова'")
+            "2" -> showStatistics(dictionary)
+            "0" -> break
+            else -> println("Введите число 1, 2 или 0")
+        }
+    }
+}
+
+fun loadDictionary(): List<Word> {
     val wordsFile = File("words.txt")
     val dictionary = mutableListOf<Word>()
 
@@ -12,9 +31,21 @@ fun main() {
         dictionary.add(Word(original, translation, correctAnswersCount))
     }
     dictionary.forEach { println("${it.original} - ${it.translation} - ${it.correctAnswersCount}") }
+    return dictionary
 }
+
+fun showStatistics(dictionary: List<Word>) {
+    val totalCount = dictionary.size
+    val learnedCount = dictionary.count { it.correctAnswersCount >= 3 }
+    val percent = (learnedCount.toDouble() / totalCount * 100).toInt()
+
+    println("Выучено $learnedCount из $totalCount слов | $percent%")
+    println()
+}
+
 data class Word(
     val original: String,
     val translation: String,
-    var correctAnswersCount:Int = 0
+    var correctAnswersCount: Int = 0
 )
+
